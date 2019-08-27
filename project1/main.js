9af3ce80b22583f44a6b8d3c94865421ccc3297e
Fatemeh Pakpour <fatemehpakpour64@gmail.com>
2019-08-27 08:23:53 +0200

    added the vue learning project

---
+++ project1/main.js
@@ -0,0 +1,26 @@
+const titleElemnt = new Vue ({
+    el:"#title" ,
+    data: {
+        title :"HELLO VUE!!!",
+        message : "I love learning vue.js"
+    }
+
+})
+
+const appElement = new Vue({
+    el : "#app" ,
+    data :{
+        headline:"Teacher Profile" ,
+        name:"Fatemeh" ,
+       subject :"Vue.js"
+    }
+})
+const book = new Vue ({
+    el: "#book" ,
+    data: {
+        title: "alchemist"
+        author:
+        summary:
+    }
+
+})
